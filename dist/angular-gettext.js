de6c99565c2d68de2cf677b48b5b3b4d1dfa909f
Martin Thorsen Ranang <mtr@ranang.org>
2014-11-14 15:39:54 +0100

    Added a new, interpolating version of the translate directive: translate-dynamic.  Enables stuff like <span translate="{{ item.domain }}">{{ item.string }}</span>.

--- dist/angular-gettext.js
+++ dist/angular-gettext.js
@@ -109,6 +109,79 @@ angular.module('gettext').factory('gettextCatalog', ["gettextPlurals", "$http", 
     return catalog;
 }]);
 
+angular.module('gettext').directive('translateDynamic', ["gettextCatalog", "$parse", "$animate", "$compile", "$interpolate", function (gettextCatalog, $parse, $animate, $compile, $interpolate) {
+    // Trim polyfill for old browsers (instead of jQuery)
+    // Based on AngularJS-v1.2.2 (angular.js#620)
+    var trim = (function () {
+        if (!String.prototype.trim) {
+            return function (value) {
+                return (typeof value === 'string') ? value.replace(/^\s*/, '').replace(/\s*$/, '') : value;
+            };
+        }
+        return function (value) {
+            return (typeof value === 'string') ? value.trim() : value;
+        };
+    })();
+
+    function assert(condition, missing, found) {
+        if (!condition) {
+            throw new Error('You should add a ' + missing + ' attribute whenever you add a ' + found + ' attribute.');
+        }
+    }
+
+    return {
+        restrict: 'A',
+        terminal: true,
+        compile: function compile(element, attrs) {
+            // Validate attributes
+            assert(!attrs.translatePlural || attrs.translateN, 'translate-n', 'translate-plural');
+            assert(!attrs.translateN || attrs.translatePlural, 'translate-plural', 'translate-n');
+
+            var msgidString = trim(element.html());
+            var translatePlural = attrs.translatePlural;
+            var translateDomainString = attrs.translateDynamic || attrs.translateDomain;
+
+            return {
+                post: function (scope, element, attrs) {
+                    var countFn = $parse(attrs.translateN);
+                    var pluralScope = null;
+                    var msgid = $interpolate(msgidString)(scope);
+                    var translateDomain = $interpolate(translateDomainString)(scope);
+
+                    function update() {
+                        // Fetch correct translated string.
+                        var translated;
+
+                        if (translatePlural) {
+                            scope = pluralScope || (pluralScope = scope.$new());
+                            scope.$count = countFn(scope);
+                            translated = gettextCatalog.getPlural(scope.$count, msgid, translatePlural, null, translateDomain);
+                        } else {
+                            translated = gettextCatalog.getString(msgid, null, translateDomain);
+                        }
+
+                        // Swap in the translation
+                        var newWrapper = angular.element('<span>' + translated + '</span>');
+                        $compile(newWrapper.contents())(scope);
+                        var oldContents = element.contents();
+                        var newContents = newWrapper.contents();
+                        $animate.enter(newContents, element);
+                        $animate.leave(oldContents);
+                    }
+
+                    if (attrs.translateN) {
+                        scope.$watch(attrs.translateN, update);
+                    }
+
+                    scope.$on('gettextLanguageChanged', update);
+
+                    update();
+                }
+            };
+        }
+    };
+}]);
+
 angular.module('gettext').directive('translate', ["gettextCatalog", "$parse", "$animate", "$compile", function (gettextCatalog, $parse, $animate, $compile) {
     // Trim polyfill for old browsers (instead of jQuery)
     // Based on AngularJS-v1.2.2 (angular.js#620)
